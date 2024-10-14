7364ef038625d04d1f45ee695392839a9f1391ed
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-14 13:00:54 -0400

    added intro to MovieSearchPage.jsx

--- client/src/pages/MovieSearchPage.jsx
+++ client/src/pages/MovieSearchPage.jsx
@@ -10,6 +10,10 @@ const MovieSearchPage = () => {
 
     return (
         <section className="form-section">
+            <div className="mb-5">
+                <h1 className="text-3xl text-center mb-5">Lets's pick a movie</h1>
+                <p className="text-xl mb-5">Fill out the form below, AI will do the rest</p>
+            </div>
             <MovieFormComponent
                 input={inputFields}
                 buttonText="Let's Go"
