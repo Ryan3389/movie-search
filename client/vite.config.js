08b4c03cc876eb5f208405356cac39341da4f375
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-16 13:48:25 -0400

    vite config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/api': {
+//         target: 'https://movie-search-zzyc.onrender.com/',
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     }
+//   }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
     open: true,
@@ -15,4 +38,5 @@ export default defineConfig({
       }
     }
   }
-})
+});
+
