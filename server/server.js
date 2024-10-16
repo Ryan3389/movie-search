96d8654238e46300e6b3bf31228b232dc2e8d5b4
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-16 14:23:19 -0400

    fixed server dist path

--- server/server.js
+++ server/server.js
@@ -7,11 +7,11 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(PORT, () => {
