386a0d475047ee0bbc3a226634a15118227ed267
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-16 13:12:00 -0400

    updated vite.config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://movie-search-zzyc.onrender.com/',
         changeOrigin: true,
         secure: false
       }
