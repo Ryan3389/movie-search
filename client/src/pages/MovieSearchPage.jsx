4b83009f8d63131bb26c91460d7b3be4181dad90
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-16 14:44:30 -0400

    updated fetch url in movieSearchPage

--- client/src/pages/MovieSearchPage.jsx
+++ client/src/pages/MovieSearchPage.jsx
@@ -23,8 +23,9 @@ const MovieSearchPage = () => {
     const handleFormSubmit = async (e) => {
         e.preventDefault();
         setLoading(true)
+        const url = import.meta.env.MODE === 'production' ? 'https://movie-search-zzyc.onrender.com/api/movie/query' : '/api/movie/query'
         try {
-            const response = await fetch("/api/movie/query", {
+            const response = await fetch(url, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
