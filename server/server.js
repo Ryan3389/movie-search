71a2a854bcc8d07a3aa719a4613b9b9e3eb41f7f
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-16 12:44:17 -0400

    updated sever

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const routes = require('./routes')
-
-const PORT = process.env.port || 3001
+const path = require('path')
+const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
