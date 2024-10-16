dd972ae4eec1f91cc65d371d7052ee3a3474f4f6
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-16 16:42:07 -0400

    updated files for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
     open: true,
@@ -11,32 +15,12 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+    },
+  },
 
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+  appType: 'spa',
+});
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/',
-//   build: {
-//     outDir: 'dist',
-//   },
-//   server: {
-//     port: 3000,
-//     open: true,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:3001',
-//         changeOrigin: true,
-//         secure: false
-//       }
-//     }
-//   }
-// });
 
